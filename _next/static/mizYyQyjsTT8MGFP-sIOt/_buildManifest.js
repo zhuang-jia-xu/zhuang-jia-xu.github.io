7d3bc9a3fab8130dd884a68961495868665b8513
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2022-04-10 15:54:04 +0800

    clear gh-pages branch

--- _next/static/mizYyQyjsTT8MGFP-sIOt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-ef39b8668b8b9af0.js","static/css/8c950ed8af5a52d2.css","static/chunks/pages/index-b406dcc3fb3089d4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
