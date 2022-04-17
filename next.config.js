e69d2a9cb99240a657beff49be576a3661fd81ba
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2022-04-17 22:46:47 +0800

    trying parallax effect

--- next.config.js
+++ next.config.js
@@ -3,12 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  exportPathMap: () => ({
-    "/": { page: "/" },
-  }),
+  // exportPathMap: () => ({
+  //   "/": { page: "/" },
+  // }),
   images: {
-    loader: "akamai",
-    path: "/",
+    domains: ["loremflickr.com"],
   },
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
 import type { NextPage } from "next";
+import Image from "next/image";
+
 import { ParticleText } from "components";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
+import { faker } from "@faker-js/faker";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <ParticleText text={"Max\nChuang"} />
-    </div>
+    <Parallax pages={2}>
+      <ParallaxLayer offset={0} speed={0.2} factor={3}>
+        {/* background */}
+        <div style={{ backgroundColor: "#000", width: "100%", height: "100%" }}>
+          <div style={{ height: "100vh" }}>
+            <ParticleText text={"Max\nChuang"} />
+          </div>
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={0.8} speed={1}>
+        <Image alt="test" src={faker.image.abstract()} layout="fill" />
+      </ParallaxLayer>
+    </Parallax>
   );
 };
 
