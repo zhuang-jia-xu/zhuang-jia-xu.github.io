6f2d4091fe077ca116dc1965e5abf8deaae0c4c2
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2022-04-19 02:44:59 +0800

    add chevron

---
+++ components/BouncingChevron/BouncingChevron.tsx
@@ -0,0 +1,6 @@
+import { useSpring, animated } from "react-spring";
+import { BsChevronDown } from "react-icons/bs";
+
+export const BouncingChevron = () => {
+  return <BsChevronDown fontSize="100px" color="white" />;
+};
