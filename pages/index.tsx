2b5aa00a397931d3a0d0366a6298743c17512fd7
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2022-04-18 21:26:30 +0800

    add paralax layer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Image from "next/image";
+import Head from "next/head";
 
 import { ParticleText } from "components";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
@@ -8,19 +9,26 @@ import { faker } from "@faker-js/faker";
 
 const Home: NextPage = () => {
   return (
-    <Parallax pages={2}>
-      <ParallaxLayer offset={0} speed={0.2} factor={3}>
-        {/* background */}
-        <div style={{ backgroundColor: "#000", width: "100%", height: "100%" }}>
-          <div style={{ height: "100vh" }}>
-            <ParticleText text={"Max\nChuang"} />
+    <>
+      <Head>
+        <title>Chuang Jia Xu</title>
+      </Head>
+      <Parallax pages={2}>
+        <ParallaxLayer offset={0} speed={0.2} factor={3}>
+          {/* background */}
+          <div
+            style={{ backgroundColor: "#000", width: "100%", height: "100%" }}
+          >
+            <div style={{ height: "100vh" }}>
+              <ParticleText text={"Max\nChuang"} />
+            </div>
           </div>
-        </div>
-      </ParallaxLayer>
-      <ParallaxLayer offset={0.8} speed={1}>
-        <Image alt="test" src={faker.image.abstract()} layout="fill" />
-      </ParallaxLayer>
-    </Parallax>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.8} speed={1}>
+          <Image alt="test" src={faker.image.abstract()} layout="fill" />
+        </ParallaxLayer>
+      </Parallax>
+    </>
   );
 };
 
