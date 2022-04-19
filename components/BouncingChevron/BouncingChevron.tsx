b669d09bdb2a2c0a3ed240f7aae56f217b26fd14
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2022-04-19 13:20:07 +0800

    add bouncing chevron

--- components/BouncingChevron/BouncingChevron.tsx
+++ components/BouncingChevron/BouncingChevron.tsx
@@ -1,6 +1,21 @@
-import { useSpring, animated } from "react-spring";
+import { useSpring, animated, easings } from "react-spring";
 import { BsChevronDown } from "react-icons/bs";
 
 export const BouncingChevron = () => {
-  return <BsChevronDown fontSize="100px" color="white" />;
+  const bounceStyle = useSpring({
+    config: {
+      duration: 1500,
+      easing: easings.easeInOutCubic,
+    },
+    loop: {
+      reverse: true,
+    },
+    from: { top: 10 },
+    to: { top: -10 },
+  });
+  return (
+    <animated.div className="relative opacity-50" style={bounceStyle}>
+      <BsChevronDown fontSize="100px" color="white" />
+    </animated.div>
+  );
 };
